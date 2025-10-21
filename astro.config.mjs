50da8710b18a54cb85a528649afc5a1b5a5ccfea
Zhang-Jingdian <2157429750@qq.com>
2025-10-21 14:28:03 +0800

    feat：更改站点url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // 生产环境的站点 URL
-  site: 'https://zhang-jingdian-github-5drsehnc3-zjds-projects-6160e985.vercel.app',
+  site: 'https://zhang-jingdian-github-io.vercel.app/',
   
   vite: {
       plugins: [tailwindcss()],
